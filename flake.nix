81e369e4657773012c7c5bae6cd154b2ffb3bbfa
david <amouyaljerome@gmail.com>
2026-05-10 20:27:04 +0200

    fix: flake - store path and read at runtime

--- flake.nix
+++ flake.nix
@@ -10,15 +10,14 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-      in
-      {
-        packages.omnigraph = pkgs.stdenv.mkDerivation {
+        
+        omnigraph-store-path = pkgs.stdenv.mkDerivation {
           pname = "omnigraph";
           version = "0.2.0";
           src = self;
           
           installPhase = ''
-            mkdir -p $out/bin $out/share/omnigraph
+            mkdir -p $out/share/omnigraph
             cp omnigraph.ts $out/share/omnigraph/
             cp package.json $out/share/omnigraph/
             
@@ -27,13 +26,15 @@
               cp -r node_modules $out/share/omnigraph/
             fi
             
-            cat > $out/bin/omnigraph << 'EOF'
-#!/bin/sh
-exec bun run $out/share/omnigraph/omnigraph.ts "$@"
-EOF
-            chmod +x $out/bin/omnigraph
+            echo $out > $out/share/omnigraph-path
           '';
         };
+      in
+      {
+        packages.omnigraph = pkgs.writeShellScriptBin "omnigraph" ''
+          OMNIGRAPH_PATH=$(cat ${omnigraph-store-path}/share/omnigraph-path)
+          exec ${pkgs.bun}/bin/bun run "$OMNIGRAPH_PATH/share/omnigraph/omnigraph.ts" "$@"
+        '';
 
         packages.default = self.packages.${system}.omnigraph;
 
